352e77acb4fa9cb56cfd8aa6b0b59fdcf1b45c17
Numan <numan.mercan24@gmail.com>
2026-08-11 23:44:32 +0300

    new: agents

--- Phase-4/anthropic_app.py
+++ Phase-4/anthropic_app.py
@@ -4,12 +4,12 @@ from anthropic import Anthropic
 
 load_dotenv()
 api_key = os.environ["ANTHROPIC_API_KEY"]
-
+print(type(os.getenv("MAX_TOKENS")))
 client = Anthropic()
 
 response = client.messages.create(
     model="claude-sonnet-4-5",
-    max_tokens=1024,
+    max_tokens=10,
     system="Sen alanında uzman bir Fullstack Developer ve Architectsin.",
     messages=[
         {"role": "user", "content": "Merhaba, kısaca kendini tanıt."}
